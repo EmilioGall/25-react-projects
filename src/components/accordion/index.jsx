4afb8c27b99d51f373217bd0588684fa8b1e1f83
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-19 19:38:23 +0100

    feat: added html structure and logic of accordion single selection - components/accordion/index.jsx

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,10 +1,65 @@
+import { useState } from "react";
+import data from './data';
+
 export default function Accordion() {
+
+   const [selected, setSelected] = useState(null);
+
+   function handleSingleSelection(getCurrentId) {
+
+      console.log(getCurrentId);
+
+      setSelected(getCurrentId == selected ? null : getCurrentId);
+
+   }
+
 
    return (
 
-      <div>
+      <div className="wrapper">
 
          <h1>Accordion</h1>
+
+         <div className="accordion">
+            {
+
+               data && data.length > 0 ?
+
+                  data.map((dataItem) =>
+
+                     <div className="item">
+
+                        <div className="title" onClick={() => handleSingleSelection(dataItem.id)}>
+
+                           <h3>{dataItem.question}</h3>
+
+                           <span>+</span>
+
+                        </div>
+
+                        {
+
+                           selected == dataItem.id ?
+
+                              <div>
+
+                                 {dataItem.answer}
+
+                              </div>
+                              :
+
+                              null
+
+                        }
+
+                     </div>
+
+                  ) :
+
+                  <div>No Data found!</div>
+
+            }
+         </div>
 
       </div>
 
