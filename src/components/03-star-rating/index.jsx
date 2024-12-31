2dc73999ad3bc581e475f21a080b2216fdd4186d
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-31 17:08:05 +0100

    feat: created general logic structure for stars click/hover - components/03-star-rating/index.jsx

--- src/components/03-star-rating/index.jsx
+++ src/components/03-star-rating/index.jsx
@@ -1,16 +1,49 @@
 import { useState } from "react";
+import { FaStar } from 'react-icons/fa';
 import './style.css';
 
-export default function ColorGenerator() {
+export default function StarRating({ starsNum = 5 }) {
 
-   const [colorType, setColorType] = useState('rgba')
+   const [rating, setRating] = useState(0);
+   const [hoverRating, setHoverRating] = useState(0);
+
+   function handleClick(currentStarIndex) {
+
+      console.log(currentStarIndex);
+
+   }
+
+   function handleMouseHover(currentStarIndex) {
+
+      console.log(currentStarIndex);
+
+   }
+
+   function handleMouseLeave(currentStarIndex) {
+      
+      console.log(currentStarIndex);
+
+   }
 
    return (
 
       <div className="h-screen w-full p-10 bg-red-800">
+         {
 
+            [...Array(starsNum)].map((value, index) => {
 
+               const starNum = index + 1;
 
+               return <FaStar
+                  key={index}
+                  onClick={() => { handleClick(starNum) }}
+                  onMouseMove={() => { handleMouseHover(starNum) }}
+                  onMouseLeave={() => { handleMouseLeave(starNum) }}
+                  size={40}
+               />
+            })
+
+         }
       </div>
 
    );
