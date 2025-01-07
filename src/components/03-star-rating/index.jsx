b5befcbc9e89858824a3775a2c98bf408319e1fe
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-07 22:41:47 +0100

    feat: adjusted responsive style of stars - components/03-star-rating/index.jsx

--- src/components/03-star-rating/index.jsx
+++ src/components/03-star-rating/index.jsx
@@ -36,7 +36,7 @@ export default function StarRating({ starsNum = 5 }) {
 
          <h2 className="text-4xl text-center font-bold">Star rating</h2>
 
-         <div className="flex justify-center items-center gap-2">
+         <div className="flex justify-center items-center gap-1 sm:gap-2">
 
             {
 
@@ -46,7 +46,7 @@ export default function StarRating({ starsNum = 5 }) {
 
                   return <FaStar
                      key={index}
-                     className={starNum <= (hoverRating || rating) ? 'text-yellow-500' : 'text-slate-500'}
+                     className={`size-6 sm:size-8 lg:size-10 ${starNum <= (hoverRating || rating) ? 'text-yellow-500' : 'text-slate-500'}`}
                      onClick={() => { handleClick(starNum) }}
                      onMouseMove={() => { handleMouseHover(starNum) }}
                      onMouseLeave={() => { handleMouseLeave(starNum) }}
