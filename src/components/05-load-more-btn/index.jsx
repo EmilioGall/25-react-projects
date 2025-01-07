b1097a2e99e2ea358ea1755d264f39b2a08e488d
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-07 22:46:32 +0100

    feat: added one more option in select tags - components/05-load-more-btn/index.jsx

--- src/components/05-load-more-btn/index.jsx
+++ src/components/05-load-more-btn/index.jsx
@@ -86,6 +86,7 @@ export default function LoadMoreBtn({ url }) {
                <option value="10">10</option>
                <option value="15">15</option>
                <option value="20">20</option>
+               <option value="50">50</option>
             </select>
 
          </div>
@@ -121,6 +122,7 @@ export default function LoadMoreBtn({ url }) {
                <option value="10">10</option>
                <option value="15">15</option>
                <option value="20">20</option>
+               <option value="50">50</option>
             </select>
 
          </div>
