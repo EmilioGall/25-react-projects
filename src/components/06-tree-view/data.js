2073a0425285d175c9dcfbfab1e199ffc7f5ce2d
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-07 16:17:36 +0100

    feat: added data structure for side menu - components/06-tree-view/data.js

---
+++ src/components/06-tree-view/data.js
@@ -0,0 +1,49 @@
+export const sideMenu = [
+
+   {
+      number: '01',
+      title: 'Accordion',
+      to: '/'
+   },
+
+   {
+      number: '02',
+      title: 'Random color generator',
+      to: '/'
+   },
+
+   {
+      number: '03',
+      title: 'Star rating',
+      to: '/'
+   },
+
+   {
+      number: '04',
+      title: 'Image Slider',
+      to: '/'
+   },
+
+   {
+      number: '05',
+      title: 'Load-more button',
+      to: '/',
+      children: [
+         {
+            title: 'Start',
+            to: '/',
+         },
+         {
+            title: 'End',
+            to: '/',
+         }
+      ]
+   },
+
+   {
+      number: '06',
+      title: 'Tree view',
+      to: '/'
+   },
+
+];
