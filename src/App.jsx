0b0c9bfedfaff9963f036d519f816a59e016e708
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-09 18:02:20 +0100

    feat: added Scroll Indicator component in main app - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import sideMenu from './components/06-tree-view/data'; // Import data for the si
 import { RxHamburgerMenu } from "react-icons/rx"; // Import hamburger menu icon from React Icons
 import QrCodeGenerator from './components/07-qr-code-generator/index';
 import LightDarkMode from './components/08-light-dark-mode/index';
+import ScrollIndicator from './components/09-scroll-indicator/index';
 
 // Define the main App component
 function App() {
@@ -33,7 +34,7 @@ function App() {
       <section>
 
         {/* SideBar - initially hidden off-screen on smaller screens */}
-        <div className={`fixed top-0 left-0 z-40 h-screen transition-all -translate-x-full sm:translate-x-0 w-20 md:w-56 bg-slate-500 ${sideBarOn ? 'opacity-80 translate-x-0' : ''}`}>
+        <div className={`fixed top-0 left-0 z-30 h-screen transition-all -translate-x-full sm:translate-x-0 w-20 md:w-56 bg-slate-500 ${sideBarOn ? 'opacity-80 translate-x-0' : ''}`}>
 
           {/* Tree View Component */}
           <TreeView
@@ -42,8 +43,16 @@ function App() {
 
         </div>
 
+        {/* SideBar - initially hidden off-screen on smaller screens */}
+        <div className={`fixed top-0 left-2 z-40 h-screen transition-all`}>
+
+          {/* Tree View Component */}
+          <ScrollIndicator />
+
+        </div>
+
         {/* Button to toggle the visibility of the sidebar */}
-        <button className={`fixed top-3 z-40 p-4 transition-all sm:ml-20 md:ml-56 sm:hidden text-center text-slate-300 bg-slate-500 opacity-80 ${sideBarOn ? 'translate-x-20 left-0 rounded-e-lg' : 'left-3 rounded-lg'}`} onClick={handleToogleSideBar}>
+        <button className={`fixed top-3 z-30 p-4 transition-all sm:ml-20 md:ml-56 sm:hidden text-center text-slate-300 bg-slate-500 opacity-80 ${sideBarOn ? 'translate-x-20 left-0 rounded-e-lg' : 'left-3 rounded-lg'}`} onClick={handleToogleSideBar}>
 
           <RxHamburgerMenu className="font-bold text-2xl text-slate-100" />
 
