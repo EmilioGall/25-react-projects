0deecc8c81581e53a677a611d4a5a1ce99234e38
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-09 19:34:05 +0100

    feat: adjusted raw style of scroll indicator - src/components/09-scroll-indicator/index.jsx

--- src/components/09-scroll-indicator/index.jsx
+++ src/components/09-scroll-indicator/index.jsx
@@ -3,14 +3,14 @@ export default function ScrollIndicator() {
    return (
 
       // Main section of the Scroll Indicator component
-      <section id="scroll-indicator" className={`h-screen w-5 pt-12 font-mono`}>
+      <section id="scroll-indicator" className={`h-screen w-4 pt-12 font-mono`}>
 
          <div className={`h-full py-5`}>
 
-            <div className={`h-full w-full bg-rose-400 rounded-full flex flex-col justify-center items-center`}>
+            <div className={`h-full w-full bg-slate-600 rounded-full flex flex-col justify-center items-center`}>
 
                {/* Scroll Indicator component Title */}
-               <h2 className={`text-lg rotate-90 text-center text-nowrap font-bold`}>Scroll Indicator</h2>
+               <h2 className={`text-sm text-slate-100 rotate-90 text-center text-nowrap`}>Scroll Indicator</h2>
 
             </div>
 
