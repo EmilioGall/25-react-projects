26b14424090f303d91742cdea7ac5feb76b17541
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-09 20:50:50 +0100

    feat: wrapped App in StoreProvider - src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; // Import Tailwind CSS
 import App from './App.jsx';
+import { StoreProvider } from './store'; // Import the StoreProvider
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
 
-    <App />
+    <StoreProvider> {/* Wrap the App component with StoreProvider */}
+
+      <App />
+
+    </StoreProvider>
 
   </StrictMode>,
 
