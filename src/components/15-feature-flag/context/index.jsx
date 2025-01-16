fd691d1f5e8a7fbca08966bf56e00361a5a005f7
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-16 16:17:55 +0100

    feat: created base context for Feature Flag component - src/components/15-feature-flag/context/index.jsx

---
+++ src/components/15-feature-flag/context/index.jsx
@@ -0,0 +1,43 @@
+import { createContext, useEffect, useState } from "react";
+import featureFlagDataServiceCall from '../data'
+
+export const FeatureFlagContext = createContext(null);
+
+export default function FeatureFlagGlobalState({ children }) {
+
+   const [loading, setLoading] = useState(false);
+   const [enabledFlags, setEnabledFlags] = useState({});
+
+   async function fetchEnabledFlags() {
+
+      try {
+
+         const response = await featureFlagDataServiceCall;
+
+         console.log(response);
+
+         setEnabledFlags(response);
+
+      } catch (error) {
+
+         console.log(error);
+
+         throw new Error(error);
+
+      };
+
+   };
+
+   useEffect(() => {
+
+      fetchEnabledFlags();
+
+   }, [])
+
+   return <FeatureFlagContext.Provider value={{}}>
+
+      {children}
+
+   </FeatureFlagContext.Provider>
+
+};
