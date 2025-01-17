8d93faf4b9d0e414b71e9da14e8ab6d7ad95619a
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-17 16:35:27 +0100

    feat: created and implemented custom hook - src/components/17-use-outside-click-custom-hook/index.jsx

--- src/components/17-use-outside-click-custom-hook/index.jsx
+++ src/components/17-use-outside-click-custom-hook/index.jsx
@@ -1,4 +1,30 @@
-export default function useOnClickOutside() {
+import { useEffect } from "react";
 
+export default function useOnClickOutside(ref, handler) {
 
+   useEffect(() => {
+
+      function listener(event) {
+
+         if (!ref.current || ref.current.contains(event.target)) {
+
+            return
+
+         };
+
+         handler(event);
+
+      };
+
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
+
+      return () => {
+
+         document.removeEventListener('mousedown', listener);
+         document.removeEventListener('touchstart', listener);
+
+      };
+
+   }, [ref, handler])
 };
