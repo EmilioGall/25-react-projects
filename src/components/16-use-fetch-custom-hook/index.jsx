b2eac038067e60a489fc74e92094d73944f27f0f
Emilio Gallo <emilio.gallo17@gmail.com>
2025-01-17 10:58:11 +0100

    feat: implemented custom hook for fetch data - src/components/16-use-fetch-custom-hook/index.jsx

--- src/components/16-use-fetch-custom-hook/index.jsx
+++ src/components/16-use-fetch-custom-hook/index.jsx
@@ -1,4 +1,46 @@
-export default function useFetch(url, options={}) {
+import { useEffect, useState } from "react";
+
+export default function useFetch(url, options = {}) {
+
+   const [error, setError] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const [data, setData] = useState(null);
 
 
+   async function fetchData() {
+
+      setLoading(true);
+
+      try {
+
+         const response = await fetch(url, { ...options });
+
+         if (!response.ok) throw new Error(response.statusText);
+
+         const result = await response.json();
+
+         setData(result);
+
+         setError(null);
+
+      } catch (e) {
+
+         setError(`Error occured: ${e}`);
+
+      } finally {
+
+         setLoading(false);
+
+      };
+
+   };
+
+   useEffect(() => {
+
+      fetchData();
+
+   }, [url]);
+
+   return { error, loading, data };
+   
 };
